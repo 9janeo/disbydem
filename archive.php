66f5f15ccf5170c2891a504d6b1981fc6e20afbe
Matt Adewole <matt.adewole@gmail.com>
2022-12-11 20:06:46 -0600

    add footer and archive templates to child theme

---
+++ archive.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+
+<div class="wrapper" id="archive-wrapper">
+
+	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+
+		<div class="row">
+
+			<!-- Do the left sidebar check -->
+			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
+
+			<main class="site-main" id="main">
+
+				<?php
+				if ( have_posts() ) {
+					?>
+					<header class="page-header">
+						<?php
+						the_archive_title( '<h1 class="page-title">', '</h1>' );
+						the_archive_description( '<div class="taxonomy-description">', '</div>' );
+						?>
+					</header><!-- .page-header -->
+					<div class="posts-list">
+						<?php while ( have_posts() ) { ?>
+							<div class="col-lg-4 col-md-6">
+								<?php the_post(); 
+								get_template_part( 'loop-templates/content', 'card' ); ?>
+							</div>
+						<?php } ?>
+					</div>
+				<?php
+				} else {
+					get_template_part( 'loop-templates/content', 'none' );
+				}
+				?>
+
+			</main><!-- #main -->
+
+			<?php
+			// Display the pagination component.
+			understrap_pagination();
+			// Do the right sidebar check.
+			get_template_part( 'global-templates/right-sidebar-check' );
+			?>
+
+		</div><!-- .row -->
+
+	</div><!-- #content -->
+
+</div><!-- #archive-wrapper -->
+
+<?php
+get_footer();
