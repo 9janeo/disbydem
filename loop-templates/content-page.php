d68384f41a709dbeff12ff88518175e3e92e6533
Matt Adewole <matt.adewole@gmail.com>
2023-01-13 18:40:08 -0600

    add page header styles, refine dropdown styles

--- loop-templates/content-page.php
+++ loop-templates/content-page.php
@@ -13,13 +13,12 @@ defined( 'ABSPATH' ) || exit;
 
 	<?php if(!(is_front_page())) : ?>
 		<header class="entry-header page">
+			<div class="banner" style="background-image: url(<?php echo get_the_post_thumbnail_url( $post->ID, 'large' ); ?>)">
+				<?php //echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+			</div>
 			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 		</header><!-- .entry-header -->
 	<?php endif; ?>
-
-
-
-	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
 
 	<div class="entry-content">
 
